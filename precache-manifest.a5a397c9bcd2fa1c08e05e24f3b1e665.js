32f257ef1e16096f5afb4fb5359fe9d6132af107
Monika Fac <monika.fac@gmail.com>
2019-01-29 17:15:52 +0100

    Updates

---
+++ precache-manifest.a5a397c9bcd2fa1c08e05e24f3b1e665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d77676ba3c003343cb0",
+    "url": "/first-shopping-app/static/js/runtime~main.7d77676b.js"
+  },
+  {
+    "revision": "c1b97adf28c05c637f76",
+    "url": "/first-shopping-app/static/js/main.c1b97adf.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/first-shopping-app/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "c1b97adf28c05c637f76",
+    "url": "/first-shopping-app/static/css/main.f6319a13.chunk.css"
+  },
+  {
+    "revision": "f314cc34b3a54ad4bfab85c1151683b0",
+    "url": "/first-shopping-app/index.html"
+  }
+];
