40c61fb3c7c9b7fae70043eadbe1f4cdcf0e6dc7
Monika Fac <monika.fac@gmail.com>
2019-01-29 17:17:14 +0100

    Add new componnets

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+
+
+class App extends Component {
+  state={
+   productsAvaliable:10,
+   amount:0,
+  
+  }
+
+ handleRemoveFormCard=() =>{
+ this.setState({
+   amount: this.state.amount - 1,
+  
+ })
+ }
+
+  handleAddToCard=() =>{
+    if (this.state.amount >= this.state.productsAvaliable){
+      this.setState({
+        amount: `10 jabłek, nie mamy już `
+      })
+    } else
+    this.setState({
+      amount: this.state.amount + 1,
+      
+    })
+  }
+  
+  handleReset = () =>{
+    this.setState({
+      amount: 0
+    })
+  }
+  
+  render() {
+    return (
+      <div className="app">
+      <div><img src="img/apple.jpg" alt="" /></div>
+      <h1>Kupiłeś: {this.state.amount} jabłek! </h1>
+      <button disabled={this.state.amount ? false: true} onClick={this.handleRemoveFormCard}>Usuń</button>
+      <button disabled={this.state.productsAvaliable===this.state.amount ? true : false} onClick={this.handleAddToCard}>Dodaj</button>
+      <button onClick={this.handleReset}>Reset</button>
+      
+      </div>
+      
+    );
+  }
+}
+
+
+export default App;
