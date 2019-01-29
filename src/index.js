d72bdc962afa1ae69eebdcd6ebe02fc86c10662d
Monika Fac <monika.fac@gmail.com>
2019-01-29 17:08:19 +0100

    Prepare fro github publish

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import App2 from './components/App2';
+import App3 from './components/App3';
+import App4 from './components/App4';
+
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app=[<App />, <App2 />, <App3 />, <App4 />];
+ReactDOM.render(app , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
